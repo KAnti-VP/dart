dabd5307eff2feed2d83fe4748ffbd061abca2bc
KAnti-VP <144044786+KAnti-VP@users.noreply.github.com>
2025-05-16 09:28:51 +0200

    Update cw_By3OrNotBy3ThatIsTheQuestion.dart

--- cw_By3OrNotBy3ThatIsTheQuestion.dart
+++ cw_By3OrNotBy3ThatIsTheQuestion.dart
@@ -4,9 +4,9 @@ bool divisibleByThree(String str) {
   int number = 0;
   for (String digit in str.split('')) {
     number += int.parse(digit);
-    while (number - 3 > 0) {
+    while (number - 3 >= 0) {
       number -= 3;
     } 
   }
-  return number - 3 == 0;
+  return number == 0;
 }
